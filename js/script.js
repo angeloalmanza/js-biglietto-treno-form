8178b4824690f865f300f9362b4c4026e2d4675f
angeloalmanza <angeloalmanza26@gmail.com>
2024-11-08 18:22:05 +0100

    feat: implementazione milestone 2

--- js/script.js
+++ js/script.js
@@ -13,24 +13,69 @@ const biglietto = (km, età) => {
         const prezzoTotale = km * tariffa;
         const sconto = prezzoTotale * scontoMin;
         const prezzoFinale = (prezzoTotale - sconto).toFixed(2);
-        messaggio = `Il tuo biglietto costa ${prezzoFinale} €`;
+        messaggio = `${prezzoFinale} €`;
     }else if(età >= 65){
         const prezzoTotale = km * tariffa;
         const sconto = prezzoTotale * scontoOver;
         const prezzoFinale = (prezzoTotale - sconto).toFixed(2);
-        messaggio = `Il tuo biglietto costa ${prezzoFinale} €`;
+        messaggio = `${prezzoFinale} €`;
     }else{
         const prezzoFinale = (km * tariffa).toFixed(2);
-        messaggio = `Il tuo biglietto costa ${prezzoFinale} €`;
+        messaggio = `${prezzoFinale} €`;
     }
     return messaggio;
 }
 
-// Prelevo le variabili dall'html
+/**
+ * funzione che genera un numero casuale da 1 a 9 per la carrozza
+ *
+ * @returns {number}
+ */
+function generaNumeroCasuale() {
+    return Math.floor(Math.random() * 9) + 1;
+}
+
+/**
+ * funzione che genera un numero casuale da 1 a 99999 per il CP
+ *
+ * @returns {number}
+ */
+function generaNumeroCasualeCP() {
+    return Math.floor(Math.random() * 99999) + 1;
+}
+
+// Prelevo le variabili inserite dall'utente
+const userForm = document.getElementById("user-form");
+const userName = document.getElementById("user-name");
 const userKm = document.getElementById("chilometri");
 const userAge = document.getElementById("eta");
 const userBtn = document.getElementById("btn");
+const btnReset = document.getElementById("btn-reset");
 
-userBtn.addEventListener("click", function(){
-    console.log(biglietto(userKm.value, userAge.value));
+// Prelevo le variabili della card del biglietto
+const textBiglietto = document.getElementById("text-biglietto");
+const card = document.getElementById("card");
+const userCard = document.getElementById("user-card");
+const offertaCard = document.getElementById("offerta-card");
+const carrozzaCard = document.getElementById("carrozza-card");
+const cpCard = document.getElementById("cp-card");
+const costoBigliettoCard = document.getElementById("costo-biglietto-card");
+
+userForm.addEventListener("submit", function(event){
+    event.preventDefault();
+    
+    // Prelevo i valori
+    const name = userName.value.trim();
+    const km = userKm.value.trim();
+    const age = userAge.value.trim();
+    
+    // Assegno i valori alle variabili della card
+    userCard.innerHTML = name;
+    costoBigliettoCard.innerHTML = biglietto(km, age);
+    offertaCard.innerHTML = "Biglietto Standard";
+    carrozzaCard.innerHTML = generaNumeroCasuale();
+    cpCard.innerHTML = generaNumeroCasualeCP();
+
+    textBiglietto.classList.remove("d-none");
+    card.classList.remove("d-none");
 })
